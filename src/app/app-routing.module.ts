0d9e9af0efad0ec406324ba64f337e0568596ac4
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-18 13:56:00 +0300

    It will add a route to a username not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { SearchComponent } from './search/search.component';
+import { UsernotfoundComponent } from './usernotfound/usernotfound.component';
 
 const routes: Routes = [
   { path: 'githubsearch', component: SearchComponent},
   { path: 'about', component: AboutComponent},
   { path:'**', component:SearchComponent},
+  { path: '', component:UsernotfoundComponent},
 ];
 
 @NgModule({
